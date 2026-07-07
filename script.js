a0a92a4d69c1c0804e3a849f02ab575223c30271
erinm-projects <erinisabellamcd@gmail.com>
2026-07-07 13:08:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const revealElements = document.querySelectorAll(".section, .theme-card, .price-card, .timeline-list div");
 
+revealElements.forEach((element) => {
+  element.classList.add("reveal");
+});
+
+const revealOnScroll = () => {
+  revealElements.forEach((element) => {
+    const elementTop = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (elementTop < windowHeight - 80) {
+      element.classList.add("visible");
+    }
+  });
+};
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
